01e8859088dd1a692813afa8b692e0da4d2fd8c9
Anna Rutkowska <rutkows.anna@gmail.com>
2020-09-12 18:55:30 +0200

    Updates

---
+++ precache-manifest.f6b7a92b577da5dc4d2b42626cffe8d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ad390db996fcd061fe89a92cbc41df7",
+    "url": "/to-do-app/index.html"
+  },
+  {
+    "revision": "9484aafbbf93f9a95622",
+    "url": "/to-do-app/static/css/main.654648fa.chunk.css"
+  },
+  {
+    "revision": "601f0cb33a29504d3cdc",
+    "url": "/to-do-app/static/js/2.508fb28f.chunk.js"
+  },
+  {
+    "revision": "68474a25a781230a9a35ecc384abd146",
+    "url": "/to-do-app/static/js/2.508fb28f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9484aafbbf93f9a95622",
+    "url": "/to-do-app/static/js/main.ce0fcb7b.chunk.js"
+  },
+  {
+    "revision": "4385e1bd226fdb470057",
+    "url": "/to-do-app/static/js/runtime-main.1f8cf036.js"
+  }
+]);
